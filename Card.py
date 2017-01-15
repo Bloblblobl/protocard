ee37758a6cdbdf3ab75345464618197b6e6c1fc1
Saar Sayfan <saarsayfan@gmail.com>
2017-01-14 23:58:38 -0800

    [PC2] Add more Attributes to the Card Object

--- Card.py
+++ Card.py
@@ -2,14 +2,16 @@ class Card(object):
     def __init__(self, name, descr, attack, health):
         self.name = name
         self.descr = descr
-        self.attack = attack
-        self.health = health
+        self.attack_stat = attack
+        self.attack_curr = attack
+        self.health_stat = health
+        self.health_curr = health
         self.actions = 1
         self.exhausted = False
 
     def __repr__(self):
         return "<Card name:{} descr:{} attack:{} health:{}>".format(
-            self.name, self.descr, self.attack, self.health)
+            self.name, self.descr, self.attack_curr, self.health_curr)
 
     def on_play(self):
         pass
